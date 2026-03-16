bee600cedc70435887cc94b0db0bd1f35427c734
Harsimran Kaur Warring <warring2075@gmail.com>
2026-03-16 22:10:13 +1100

    Create budget_frame.py file for budget management UI

---
+++ ui/budget_frame.py
@@ -0,0 +1,19 @@
+class BudgetFrame:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(master)
+        self.frame.pack()
+
+        self.label = tk.Label(self.frame, text="Budget Management")
+        self.label.pack()
+
+        self.budget_entry = tk.Entry(self.frame)
+        self.budget_entry.pack()
+
+        self.set_budget_button = tk.Button(self.frame, text="Set Budget", command=self.set_budget)
+        self.set_budget_button.pack()
+
+    def set_budget(self):
+        budget = self.budget_entry.get()
+        print(f"Budget set to: {budget}")
+        # Additional logic to manage the budget goes here.
